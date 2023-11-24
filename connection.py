ad7cee7871f617c3817deaa7a03329111c647036
Harshith-19 <harshith.jethu@gmail.com>
2023-11-24 12:09:36 +0530

    establishing db connection

---
+++ connection.py
@@ -0,0 +1,20 @@
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+DATABASE_NAME = os.getenv("DATABASE_NAME")
+DATABASE_USER = os.getenv("DATABASE_USER")
+DATABASE_PASSWORD = os.getenv("DATABASE_PASSWORD")
+DATABASE_HOST = os.getenv("DATABASE_HOST")
+DATABASE_PORT = os.getenv("DATABASE_PORT")
+
+def get_db_connection():
+    return psycopg2.connect(
+        dbname=DATABASE_NAME,
+        user=DATABASE_USER,
+        password=DATABASE_PASSWORD,
+        host=DATABASE_HOST,
+        port=DATABASE_PORT
+    )
